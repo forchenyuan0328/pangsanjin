c8a015a0c6656587556388d2601ba0e3e5c29fc1
pangsanjin <3356607319@qq.com>
2018-03-09 21:37:23 +0800

    hb

---
+++ js/play.js
@@ -0,0 +1,17 @@
+//音乐自动播放
+function audioAutoPlay(id){  
+	var audio = document.getElementById(id),  
+		play = function(){  
+			audio.play();  
+			document.removeEventListener("touchstart",play, false);  
+		};  
+	audio.play();  
+	document.addEventListener("WeixinJSBridgeReady", function () {  
+		play();  
+	}, false);  
+	document.addEventListener('YixinJSBridgeReady', function() {  
+		play();  
+	}, false);  
+	document.addEventListener("touchstart",play, false);  
+}  
+audioAutoPlay('Jaudio');
